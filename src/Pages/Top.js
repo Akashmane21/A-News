fa48be7499c1b5cbc0ec408cc97a68cef816f48e
Akashmame <79107396+Akashmane21@users.noreply.github.com>
2021-03-15 21:10:38 +0530

    heading fix

--- src/Pages/Top.js
+++ src/Pages/Top.js
@@ -1,16 +1,19 @@
 import '../App.css';
 import Menu from '../Menu'
-
+import logo from '../logo.png'
 function Top() {
   return (
    <>
 <Menu />
 <div className="Top">
 <img src="https://images.unsplash.com/photo-1497008386681-a7941f08011e?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MzB8fG5ld3N8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60" alt="" />
+
 <div className="heading">
-<center><h1> A-News</h1>
+<center>
+<img src={logo} alt="" />
+<h1> A-News</h1>
 <h2>Explore The World</h2>
-<p>increase the knoeledge</p></center>
+<p>increase the knowledge</p></center>
 </div>
    </div>
    </>
