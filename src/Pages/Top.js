fa4be868e61a91d49e69042be692ea0f132b6d5b
Akashmame <79107396+Akashmane21@users.noreply.github.com>
2021-03-16 22:10:18 +0530

    don

--- src/Pages/Top.js
+++ src/Pages/Top.js
@@ -1,10 +1,10 @@
 import '../App.css';
 import Menu from '../Menu'
 import { Component } from "react";
-
+import logo from '../logo.png'
 import Avatar from '@material-ui/core/Avatar';
 import SearchOutlined from '@material-ui/icons/SearchOutlined';
-
+import logo1 from '../title (1).png'
 // import Toptrend from '../Components/Toptrend';
 
 
@@ -44,26 +44,33 @@ render() {
    <>
 <Menu />
 <div className="Top">
-<img src="https://images.unsplash.com/photo-1504598210222-a29af75e1619?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTI2fHxuZXdzfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60" alt="" />
 <div className="intro">
 <Avatar src="https://instagram.fyyc6-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s640x640/90651740_569667850563489_8116038555498102848_n.jpg?tp=1&_nc_ht=instagram.fyyc6-1.fna.fbcdn.net&_nc_cat=109&_nc_ohc=4ESs9LR4HUEAX9tEKIE&oh=04b9eca23cf98a6dbd5db76029468e58&oe=607719BC" />
 <h4>Hii, I,m Akash</h4>
-</div>
+</div><img id="title" src={logo1} alt="" />
+
 <div className="heading">
 <center>
-<h1> A-News</h1>
+<div className="slogen">
+<img id="spin" src={logo} alt="" />
+<div class="slogen_child">
+Explore The World
+increase the knowledge</div>
+</div>
+{/* <h1> A-News</h1> */}<br />
+
 <div className="sidebar__search">
            <div className="sidebar__searchContainer">
                <SearchOutlined />
                <input type="text" placeholder="Search Anything..." />
            </div>
        </div>
-<h2>Explore The World</h2>
-<p>increase the knowledge</p></center>
+</center>
 </div>
    </div>
 
 <div class="lower_body">
+<h1>Top Headlines:</h1>
 {Array.isArray(data) && data.map(object => (
 
 
